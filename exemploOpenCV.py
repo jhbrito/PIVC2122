dd6feb89627835908119acb2a30d33208bdbbec2
jhasb <jbrito@ipca.pt>
2021-11-15 18:40:53 +0000

    more exemples

--- exemploOpenCV.py
+++ exemploOpenCV.py
@@ -1,5 +1,9 @@
 import cv2
 
 imagem = cv2.imread("cao.jpg")
+imagem[200:300, 400:600, 0:1] = 0
+imagem2 = imagem/255.0
+
 cv2.imshow("Imagem", imagem)
+cv2.imshow("Imagem2", imagem2)
 cv2.waitKey(0)
