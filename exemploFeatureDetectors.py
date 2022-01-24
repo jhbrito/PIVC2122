6d847fd121bfc12e97c061590f4dfe546dc141f7
jhasb <jbrito@ipca.pt>
2022-01-24 19:46:40 +0000

    ocr

--- exemploFeatureDetectors.py
+++ exemploFeatureDetectors.py
@@ -1,3 +1,5 @@
+from email.mime import image
+
 import cv2
 
 imagem = cv2.imread("Sharbat_Gula.jpg")
@@ -11,5 +13,23 @@ harris_corners = cv2.cornerHarris(imagem_gray, blockSize=2, ksize=3, k=0.04)
 imagem_harris = imagem.copy()
 imagem_harris[harris_corners > 0.01*harris_corners.max()] = [255, 0, 0]
 cv2.imshow("imagem_harris", imagem_harris)
+
+sift = cv2.SIFT_create()
+sift_kp, sift_desc = sift.detectAndCompute(imagem, None)
+imagem_sift = imagem.copy()
+imagem_sift = cv2.drawKeypoints(imagem_gray, sift_kp, None)
+cv2.imshow("imagem_sift", imagem_sift)
+
+# surf = cv2.xfeatures2d.SURF_create(400)
+# surf_kp, surf_desc = surf.detectAndCompute(imagem, None)
+# imagem_surf = imagem.copy()
+# imagem_surf = cv2.drawKeypoints(imagem_gray, surf_kp, None)
+# cv2.imshow("imagem_surf", imagem_surf)
+
+orb = cv2.ORB_create()
+orb_kp, orb_desc = orb.detectAndCompute(imagem, None)
+imagem_orb = imagem.copy()
+imagem_orb = cv2.drawKeypoints(imagem_gray, orb_kp, None)
+cv2.imshow("imagem_orb", imagem_orb)
 
 cv2.waitKey(0)
---
+++ exemploTesseract.py
@@ -0,0 +1,6 @@
+import cv2
+import pytesseract
+
+tesseract_path = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = tesseract_path
+
